3633bf0010f99a90c196d74390a91a1a4e56365c
tarikgul <tarik@parity.io>
2021-04-12 11:04:09 -0400

    revert to older version and put updates in a branch

--- src/networkRuntimes.spec.ts
+++ src/networkRuntimes.spec.ts
@@ -14,16 +14,17 @@ function request(path: string, hostname: string, port: number) {
   });
 }
 
-describe("Runtime Tests for blocks", () => {
+describe('Runtime Tests for blocks', () => {
   for (let i = 0; i < polkadotEndpoints.length; i++) {
     const blockPath = polkadotEndpoints[i];
-    const blockHeight = polkadotEndpoints[i].split("/")[2];
+    const blockHeight = polkadotEndpoints[i].split('/')[2];
 
-    it("should return a successfull request", async () => {
+    it(`should return a successfull request for block: ${blockHeight}`, async () => {
+      jest.setTimeout(15000)
       const res = await request(blockPath, `127.0.0.1`, 8080);
       const responseJson = JSON.parse(res as string);
 
-      expect(responseJson["number"]).toBe(blockHeight);
+      expect(responseJson['number']).toBe(blockHeight);
     });
   }
 });
